2dust/v2rayNG

Refactor AppConfig const

V2rayNG/app/src/main/java/com/v2ray/ang/AppConfig.kt
@@ -65,7 +65,7 @@ object AppConfig {
     const val CACHE_KEYWORD_FILTER = "cache_keyword_filter"
 
     /** Protocol identifiers. */
-    const val PROTOCOL_FREEDOM: String = "freedom"
+    const val PROTOCOL_FREEDOM = "freedom"
 
     /** Broadcast actions. */
     const val BROADCAST_ACTION_SERVICE = "com.v2ray.ang.action.service"
@@ -90,16 +90,19 @@ object AppConfig {
     const val DOWNLINK = "downlink"
 
     /** URLs for various resources. */
-    const val androidpackagenamelistUrl = "https://raw.githubusercontent.com/2dust/androidpackagenamelist/master/proxy.txt"
-    const val v2rayNGUrl = "https://github.com/2dust/v2rayNG"
-    const val v2rayNGAPIUrl = "https://api.github.com/repos/2dust/v2rayNG/releases"
-    const val v2rayNGIssues = "$v2rayNGUrl/issues"
-    const val v2rayNGWikiMode = "$v2rayNGUrl/wiki/Mode"
-    const val v2rayNGPrivacyPolicy = "https://raw.githubusercontent.com/2dust/v2rayNG/master/CR.md"
-    const val PromotionUrl = "aHR0cHM6Ly85LjIzNDQ1Ni54eXovYWJjLmh0bWw="
-    const val TgChannelUrl = "https://t.me/github_2dust"
-    const val DelayTestUrl = "https://www.gstatic.com/generate_204"
-    const val DelayTestUrl2 = "https://www.google.com/generate_204"
+    const val GITHUB_URL = "https://github.com"
+    const val GITHUB_RAW_URL = "https://raw.githubusercontent.com"
+    const val GITHUB_DOWNLOAD = "releases/latest/download"
+    const val ANDROID_PACKAGE_NAME_LIST_URL = "$GITHUB_RAW_URL/2dust/androidpackagenamelist/master/proxy.txt"
+    const val APP_URL = "$GITHUB_URL/2dust/v2rayNG"
+    const val APP_API_URL = "https://api.github.com/repos/2dust/v2rayNG/releases"
+    const val APP_ISSUES_URL = "$APP_URL/issues"
+    const val APP_WIKI_MODE = "$APP_URL/wiki/Mode"
+    const val APP_PRIVACY_POLICY = "$GITHUB_RAW_URL/2dust/v2rayNG/master/CR.md"
+    const val APP_PROMOTION_URL = "aHR0cHM6Ly85LjIzNDQ1Ni54eXovYWJjLmh0bWw="
+    const val TG_CHANNEL_URL = "https://t.me/github_2dust"
+    const val DELAY_TEST_URL = "https://www.gstatic.com/generate_204"
+    const val DELAY_TEST_URL2 = "https://www.google.com/generate_204"
 
     /** DNS server addresses. */
     const val DNS_PROXY = "1.1.1.1"
@@ -200,9 +203,9 @@ object AppConfig {
         "240.0.0.0/4"
     )
     val GEO_FILES_SOURCES = arrayListOf(
-        "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/",
-        "https://github.com/runetfreedom/russia-v2ray-rules-dat/releases/latest/download/",
-        "https://github.com/Chocolate4U/Iran-v2ray-rules/releases/latest/download/"
+        "$GITHUB_URL/Loyalsoldier/v2ray-rules-dat/$GITHUB_DOWNLOAD",
+        "$GITHUB_URL/runetfreedom/russia-v2ray-rules-dat/$GITHUB_DOWNLOAD",
+        "$GITHUB_URL/Chocolate4U/Iran-v2ray-rules/$GITHUB_DOWNLOAD"
     )
 
 }

V2rayNG/app/src/main/java/com/v2ray/ang/handler/UpdateCheckerManager.kt
@@ -7,6 +7,7 @@ import com.v2ray.ang.AppConfig
 import com.v2ray.ang.BuildConfig
 import com.v2ray.ang.dto.CheckUpdateResult
 import com.v2ray.ang.dto.GitHubRelease
+import com.v2ray.ang.extension.concatUrl
 import com.v2ray.ang.util.HttpUtil
 import com.v2ray.ang.util.JsonUtil
 import kotlinx.coroutines.Dispatchers
@@ -18,9 +19,9 @@ object UpdateCheckerManager {
     suspend fun checkForUpdate(includePreRelease: Boolean = false): CheckUpdateResult = withContext(Dispatchers.IO) {
         try {
             val url = if (includePreRelease) {
-                AppConfig.v2rayNGAPIUrl
+                AppConfig.APP_API_URL
             } else {
-                "${AppConfig.v2rayNGAPIUrl}/latest"
+                AppConfig.APP_API_URL.concatUrl("latest")
             }
 
             var response = HttpUtil.getUrlContent(url, 5000)

V2rayNG/app/src/main/java/com/v2ray/ang/ui/UserAssetActivity.kt
@@ -24,6 +24,7 @@ import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityUserAssetBinding
 import com.v2ray.ang.databinding.ItemRecyclerUserAssetBinding
 import com.v2ray.ang.dto.AssetUrlItem
+import com.v2ray.ang.extension.concatUrl
 import com.v2ray.ang.extension.toTrafficString
 import com.v2ray.ang.extension.toast
 import com.v2ray.ang.extension.toastError
@@ -121,8 +122,11 @@ class UserAssetActivity : BaseActivity() {
     }
 
     private fun setGeoFilesSources() {
-        AlertDialog.Builder(this).setItems(AppConfig.GEO_FILES_SOURCES
-            .toTypedArray()) { _, i ->
+        AlertDialog.Builder(this).setItems(
+            AppConfig.GEO_FILES_SOURCES
+                .map { it.replace(AppConfig.GITHUB_URL + "/", "").replace("/" + AppConfig.GITHUB_DOWNLOAD, "") }
+                .toTypedArray()
+        ) { _, i ->
             try {
                 val value = AppConfig.GEO_FILES_SOURCES[i]
                 MmkvManager.encodeSettings(AppConfig.PREF_GEO_FILES_SOURCES, value)
@@ -286,7 +290,8 @@ class UserAssetActivity : BaseActivity() {
                 list.add(
                     Utils.getUuid() to AssetUrlItem(
                         it,
-                        getGeoFilesSources() + it
+                        getGeoFilesSources().concatUrl(it),
+                        locked = true
                     )
                 )
             }
@@ -337,7 +342,7 @@ class UserAssetActivity : BaseActivity() {
                 holder.itemUserAssetBinding.assetProperties.text = getString(R.string.msg_file_not_found)
             }
 
-            if (item.second.remarks in builtInGeoFiles && item.second.url == getGeoFilesSources() + item.second.remarks) {
+            if (item.second.locked == true) {
                 holder.itemUserAssetBinding.layoutEdit.visibility = GONE
                 //holder.itemUserAssetBinding.layoutRemove.visibility = GONE
             } else {

parhelia512/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/AppConfig.kt
@@ -68,7 +68,7 @@ object AppConfig {
     const val CACHE_KEYWORD_FILTER = "cache_keyword_filter"
 
     /** Protocol identifiers. */
-    const val PROTOCOL_FREEDOM: String = "freedom"
+    const val PROTOCOL_FREEDOM = "freedom"
 
     /** Broadcast actions. */
     const val BROADCAST_ACTION_SERVICE = "com.neko.v2ray.action.service"
@@ -93,16 +93,19 @@ object AppConfig {
     const val DOWNLINK = "downlink"
 
     /** URLs for various resources. */
-    const val androidpackagenamelistUrl = "https://raw.githubusercontent.com/2dust/androidpackagenamelist/master/proxy.txt"
-    const val v2rayNGUrl = "https://github.com/MRT-project/Neko-ray"
-    const val v2rayNGAPIUrl = "https://api.github.com/repos/MRT-project/Neko-ray/releases"
-    const val v2rayNGIssues = "$v2rayNGUrl/issues"
-    const val v2rayNGWikiMode = "$v2rayNGUrl/wiki/Mode"
-    const val v2rayNGPrivacyPolicy = "https://raw.githubusercontent.com/MRT-project/Neko-ray/main/CR.md"
-    const val PromotionUrl = "https://9.234456.xyz/abc.html?t=1703789826882"
-    const val TgChannelUrl = "https://t.me/neko_ray_uwu"
-    const val DelayTestUrl = "https://www.gstatic.com/generate_204"
-    const val DelayTestUrl2 = "https://www.google.com/generate_204"
+    const val GITHUB_URL = "https://github.com"
+    const val GITHUB_RAW_URL = "https://raw.githubusercontent.com"
+    const val GITHUB_DOWNLOAD = "releases/latest/download"
+    const val ANDROID_PACKAGE_NAME_LIST_URL = "$GITHUB_RAW_URL/2dust/androidpackagenamelist/master/proxy.txt"
+    const val APP_URL = "$GITHUB_URL/MRT-project/Neko-ray"
+    const val APP_API_URL = "https://api.github.com/repos/MRT-project/Neko-ray/releases"
+    const val APP_ISSUES_URL = "$APP_URL/issues"
+    const val APP_WIKI_MODE = "$APP_URL/wiki/Mode"
+    const val APP_PRIVACY_POLICY = "$GITHUB_RAW_URL/MRT-project/Neko-ray/main/CR.md"
+    const val APP_PROMOTION_URL = "https://9.234456.xyz/abc.html?t=1703789826882"
+    const val TG_CHANNEL_URL = "https://t.me/neko_ray_uwu"
+    const val DELAY_TEST_URL = "https://www.gstatic.com/generate_204"
+    const val DELAY_TEST_URL2 = "https://www.google.com/generate_204"
 
     /** DNS server addresses. */
     const val DNS_PROXY = "1.1.1.1"
@@ -231,9 +234,9 @@ object AppConfig {
     )
 
     val GEO_FILES_SOURCES = arrayListOf(
-        "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/",
-        "https://github.com/runetfreedom/russia-v2ray-rules-dat/releases/latest/download/",
-        "https://github.com/Chocolate4U/Iran-v2ray-rules/releases/latest/download/",
-        "https://github.com/malikshi/v2ray-rules-dat/releases/latest/download/"
+        "$GITHUB_URL/Loyalsoldier/v2ray-rules-dat/$GITHUB_DOWNLOAD",
+        "$GITHUB_URL/runetfreedom/russia-v2ray-rules-dat/$GITHUB_DOWNLOAD",
+        "$GITHUB_URL/Chocolate4U/Iran-v2ray-rules/$GITHUB_DOWNLOAD",
+        "$GITHUB_URL/malikshi/v2ray-rules-dat/$GITHUB_DOWNLOAD"
     )
 }

V2rayNG/app/src/main/java/com/neko/v2ray/handler/UpdateCheckerManager.kt
@@ -7,6 +7,7 @@ import com.neko.v2ray.AppConfig
 import com.neko.v2ray.BuildConfig
 import com.neko.v2ray.dto.CheckUpdateResult
 import com.neko.v2ray.dto.GitHubRelease
+import com.neko.v2ray.extension.concatUrl
 import com.neko.v2ray.util.HttpUtil
 import com.neko.v2ray.util.JsonUtil
 import kotlinx.coroutines.Dispatchers
@@ -18,9 +19,9 @@ object UpdateCheckerManager {
     suspend fun checkForUpdate(includePreRelease: Boolean = false): CheckUpdateResult = withContext(Dispatchers.IO) {
         try {
             val url = if (includePreRelease) {
-                AppConfig.v2rayNGAPIUrl
+                AppConfig.APP_API_URL
             } else {
-                "${AppConfig.v2rayNGAPIUrl}/latest"
+                AppConfig.APP_API_URL.concatUrl("latest")
             }
 
             var response = HttpUtil.getUrlContent(url, 5000)

V2rayNG/app/src/main/java/com/neko/v2ray/ui/UserAssetActivity.kt
@@ -27,6 +27,7 @@ import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivityUserAssetBinding
 import com.neko.v2ray.databinding.ItemRecyclerUserAssetBinding
 import com.neko.v2ray.dto.AssetUrlItem
+import com.neko.v2ray.extension.concatUrl
 import com.neko.v2ray.extension.toTrafficString
 import com.neko.v2ray.extension.toast
 import com.neko.v2ray.extension.toastError
@@ -128,8 +129,11 @@ class UserAssetActivity : BaseActivity() {
     }
 
     private fun setGeoFilesSources() {
-        MaterialAlertDialogBuilder(this).setItems(AppConfig.GEO_FILES_SOURCES
-            .toTypedArray()) { _, i ->
+        MaterialAlertDialogBuilder(this).setItems(
+            AppConfig.GEO_FILES_SOURCES
+                .map { it.replace(AppConfig.GITHUB_URL + "/", "").replace("/" + AppConfig.GITHUB_DOWNLOAD, "") }
+                .toTypedArray()
+        ) { _, i ->
             try {
                 val value = AppConfig.GEO_FILES_SOURCES[i]
                 MmkvManager.encodeSettings(AppConfig.PREF_GEO_FILES_SOURCES, value)
@@ -293,7 +297,8 @@ class UserAssetActivity : BaseActivity() {
                 list.add(
                     Utils.getUuid() to AssetUrlItem(
                         it,
-                        getGeoFilesSources() + it
+                        getGeoFilesSources().concatUrl(it),
+                        locked = true
                     )
                 )
             }
@@ -344,7 +349,7 @@ class UserAssetActivity : BaseActivity() {
                 holder.itemUserAssetBinding.assetProperties.text = getString(R.string.msg_file_not_found)
             }
 
-            if (item.second.remarks in builtInGeoFiles && item.second.url == getGeoFilesSources() + item.second.remarks) {
+            if (item.second.locked == true) {
                 holder.itemUserAssetBinding.layoutEdit.visibility = GONE
                 //holder.itemUserAssetBinding.layoutRemove.visibility = GONE
             } else {