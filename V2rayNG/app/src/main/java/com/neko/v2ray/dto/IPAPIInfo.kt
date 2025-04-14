2dust/v2rayNG

Added IP display in connection test
https://github.com/2dust/v2rayNG/issues/4489

V2rayNG/app/src/main/java/com/v2ray/ang/dto/IPAPIInfo.kt
@@ -1,0 +1,11 @@
+package com.v2ray.ang.dto
+
+data class IPAPIInfo(
+    var ip: String? = null,
+    var city: String? = null,
+    var region: String? = null,
+    var region_code: String? = null,
+    var country: String? = null,
+    var country_name: String? = null,
+    var country_code: String? = null
+)

V2rayNG/app/src/main/java/com/v2ray/ang/handler/SpeedtestManager.kt
@@ -6,8 +6,10 @@ import android.text.TextUtils
 import android.util.Log
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.R
+import com.v2ray.ang.dto.IPAPIInfo
 import com.v2ray.ang.extension.responseLength
 import com.v2ray.ang.util.HttpUtil
+import com.v2ray.ang.util.JsonUtil
 import kotlinx.coroutines.isActive
 import libv2ray.Libv2ray
 import java.io.IOException
@@ -162,6 +164,14 @@ object SpeedtestManager {
         }
 
         return Pair(elapsed, result)
+    }
+
+    fun getRemoteIPInfo(): String? {
+        val httpPort = SettingsManager.getHttpPort()
+        var content = HttpUtil.getUrlContent(AppConfig.IP_API_Url, 5000, httpPort) ?: return null
+
+        var ipInfo = JsonUtil.fromJson(content, IPAPIInfo::class.java) ?: return null
+        return "(${ipInfo.country_code}) ${ipInfo.ip}"
     }
 
     /**

V2rayNG/app/src/main/java/com/v2ray/ang/service/V2RayServiceManager.kt
@@ -15,6 +15,7 @@ import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.extension.toast
 import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.handler.SettingsManager
+import com.v2ray.ang.handler.SpeedtestManager
 import com.v2ray.ang.handler.V2rayConfigManager
 import com.v2ray.ang.util.MessageUtil
 import com.v2ray.ang.util.PluginUtil
@@ -231,7 +232,12 @@ object V2RayServiceManager {
             val result = if (time == -1L) {
                 service.getString(R.string.connection_test_error, errstr)
             } else {
-                service.getString(R.string.connection_test_available, time)
+                buildString {
+                    append(service.getString(R.string.connection_test_available, time))
+                    SpeedtestManager.getRemoteIPInfo()?.let { ip ->
+                        append("\n$ip")
+                    }
+                }
             }
 
             MessageUtil.sendMsg2UI(service, AppConfig.MSG_MEASURE_DELAY_SUCCESS, result)

parhelia512/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/dto/IPAPIInfo.kt
@@ -1,0 +1,11 @@
+package com.neko.v2ray.dto
+
+data class IPAPIInfo(
+    var ip: String? = null,
+    var city: String? = null,
+    var region: String? = null,
+    var region_code: String? = null,
+    var country: String? = null,
+    var country_name: String? = null,
+    var country_code: String? = null
+)

V2rayNG/app/src/main/java/com/neko/v2ray/handler/SpeedtestManager.kt
@@ -6,8 +6,10 @@ import android.text.TextUtils
 import android.util.Log
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.R
+import com.neko.v2ray.dto.IPAPIInfo
 import com.neko.v2ray.extension.responseLength
 import com.neko.v2ray.util.HttpUtil
+import com.neko.v2ray.util.JsonUtil
 import kotlinx.coroutines.isActive
 import libv2ray.Libv2ray
 import java.io.IOException
@@ -128,6 +130,19 @@ object SpeedtestManager {
         return Pair(elapsed, result)
     }
 
+    fun getRemoteIPInfo(): String? {
+        val httpPort = SettingsManager.getHttpPort()
+        var content = HttpUtil.getUrlContent(AppConfig.IP_API_Url, 5000, httpPort) ?: return null
+
+        var ipInfo = JsonUtil.fromJson(content, IPAPIInfo::class.java) ?: return null
+        return "(${ipInfo.country_code}) ${ipInfo.ip}"
+    }
+
+    /**
+     * Gets the version of the V2Ray library.
+     *
+     * @return The version of the V2Ray library.
+     */
     fun getLibVersion(): String {
         return Libv2ray.checkVersionX()
     }

V2rayNG/app/src/main/java/com/neko/v2ray/service/V2RayServiceManager.kt
@@ -15,6 +15,7 @@ import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.extension.toast
 import com.neko.v2ray.handler.MmkvManager
 import com.neko.v2ray.handler.SettingsManager
+import com.neko.v2ray.handler.SpeedtestManager
 import com.neko.v2ray.handler.V2rayConfigManager
 import com.neko.v2ray.util.MessageUtil
 import com.neko.v2ray.util.PluginUtil
@@ -231,7 +232,12 @@ object V2RayServiceManager {
             val result = if (time == -1L) {
                 service.getString(R.string.connection_test_error, errstr)
             } else {
-                service.getString(R.string.connection_test_available, time)
+                buildString {
+                    append(service.getString(R.string.connection_test_available, time))
+                    SpeedtestManager.getRemoteIPInfo()?.let { ip ->
+                        append("\n$ip")
+                    }
+                }
             }
 
             MessageUtil.sendMsg2UI(service, AppConfig.MSG_MEASURE_DELAY_SUCCESS, result)