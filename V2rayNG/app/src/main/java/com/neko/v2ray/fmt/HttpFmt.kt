2dust/v2rayNG

Refactor outbound related code

V2rayNG/app/src/main/java/com/v2ray/ang/fmt/HttpFmt.kt
@@ -4,6 +4,7 @@ import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.dto.V2rayConfig.OutboundBean
 import com.v2ray.ang.extension.isNotNullEmpty
+import com.v2ray.ang.handler.V2rayConfigManager
 
 object HttpFmt : FmtBase() {
     /**
@@ -13,7 +14,7 @@ object HttpFmt : FmtBase() {
      * @return the converted OutboundBean object, or null if conversion fails
      */
     fun toOutbound(profileItem: ProfileItem): OutboundBean? {
-        val outboundBean = OutboundBean.create(EConfigType.HTTP)
+        val outboundBean = V2rayConfigManager.createOutbound(EConfigType.HTTP)
 
         outboundBean?.settings?.servers?.first()?.let { server ->
             server.address = profileItem.server.orEmpty()

V2rayNG/app/src/main/java/com/v2ray/ang/fmt/SocksFmt.kt
@@ -5,6 +5,7 @@ import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.dto.V2rayConfig.OutboundBean
 import com.v2ray.ang.extension.idnHost
 import com.v2ray.ang.extension.isNotNullEmpty
+import com.v2ray.ang.handler.V2rayConfigManager
 import com.v2ray.ang.util.Utils
 import java.net.URI
 
@@ -60,7 +61,7 @@ object SocksFmt : FmtBase() {
      * @return the converted OutboundBean object, or null if conversion fails
      */
     fun toOutbound(profileItem: ProfileItem): OutboundBean? {
-        val outboundBean = OutboundBean.create(EConfigType.SOCKS)
+        val outboundBean = V2rayConfigManager.createOutbound(EConfigType.SOCKS)
 
         outboundBean?.settings?.servers?.first()?.let { server ->
             server.address = profileItem.server.orEmpty()

V2rayNG/app/src/main/java/com/v2ray/ang/fmt/VlessFmt.kt
@@ -6,6 +6,7 @@ import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.dto.V2rayConfig.OutboundBean
 import com.v2ray.ang.extension.idnHost
 import com.v2ray.ang.handler.MmkvManager
+import com.v2ray.ang.handler.V2rayConfigManager
 import com.v2ray.ang.util.Utils
 import java.net.URI
 
@@ -56,7 +57,7 @@ object VlessFmt : FmtBase() {
      * @return the converted OutboundBean object, or null if conversion fails
      */
     fun toOutbound(profileItem: ProfileItem): OutboundBean? {
-        val outboundBean = OutboundBean.create(EConfigType.VLESS)
+        val outboundBean = V2rayConfigManager.createOutbound(EConfigType.VLESS)
 
         outboundBean?.settings?.vnext?.first()?.let { vnext ->
             vnext.address = profileItem.server.orEmpty()
@@ -67,35 +68,11 @@ object VlessFmt : FmtBase() {
         }
 
         val sni = outboundBean?.streamSettings?.let {
-            populateTransportSettings(
-                it,
-                profileItem.network.orEmpty(),
-                profileItem.headerType,
-                profileItem.host,
-                profileItem.path,
-                profileItem.seed,
-                profileItem.quicSecurity,
-                profileItem.quicKey,
-                profileItem.mode,
-                profileItem.serviceName,
-                profileItem.authority,
-                profileItem.xhttpMode,
-                profileItem.xhttpExtra
-            )
+            V2rayConfigManager.populateTransportSettings(it, profileItem)
         }
 
         outboundBean?.streamSettings?.let {
-            populateTlsSettings(
-                it,
-                profileItem.security.orEmpty(),
-                profileItem.insecure == true,
-                if (profileItem.sni.isNullOrEmpty()) sni else profileItem.sni,
-                profileItem.fingerPrint,
-                profileItem.alpn,
-                profileItem.publicKey,
-                profileItem.shortId,
-                profileItem.spiderX,
-            )
+            V2rayConfigManager.populateTlsSettings(it, profileItem, sni)
         }
 
         return outboundBean

V2rayNG/app/src/main/java/com/v2ray/ang/fmt/VmessFmt.kt
@@ -11,6 +11,7 @@ import com.v2ray.ang.dto.VmessQRCode
 import com.v2ray.ang.extension.idnHost
 import com.v2ray.ang.extension.isNotNullEmpty
 import com.v2ray.ang.handler.MmkvManager
+import com.v2ray.ang.handler.V2rayConfigManager
 import com.v2ray.ang.util.JsonUtil
 import com.v2ray.ang.util.Utils
 import java.net.URI
@@ -168,7 +169,7 @@ object VmessFmt : FmtBase() {
      * @return the converted OutboundBean object, or null if conversion fails
      */
     fun toOutbound(profileItem: ProfileItem): OutboundBean? {
-        val outboundBean = OutboundBean.create(EConfigType.VMESS)
+        val outboundBean = V2rayConfigManager.createOutbound(EConfigType.VMESS)
 
         outboundBean?.settings?.vnext?.first()?.let { vnext ->
             vnext.address = profileItem.server.orEmpty()
@@ -178,35 +179,11 @@ object VmessFmt : FmtBase() {
         }
 
         val sni = outboundBean?.streamSettings?.let {
-            populateTransportSettings(
-                it,
-                profileItem.network.orEmpty(),
-                profileItem.headerType,
-                profileItem.host,
-                profileItem.path,
-                profileItem.seed,
-                profileItem.quicSecurity,
-                profileItem.quicKey,
-                profileItem.mode,
-                profileItem.serviceName,
-                profileItem.authority,
-                profileItem.xhttpMode,
-                profileItem.xhttpExtra
-            )
+            V2rayConfigManager.populateTransportSettings(it, profileItem)
         }
 
         outboundBean?.streamSettings?.let {
-            populateTlsSettings(
-                it,
-                profileItem.security.orEmpty(),
-                profileItem.insecure == true,
-                if (profileItem.sni.isNullOrEmpty()) sni else profileItem.sni,
-                profileItem.fingerPrint,
-                profileItem.alpn,
-                null,
-                null,
-                null
-            )
+            V2rayConfigManager.populateTlsSettings(it, profileItem, sni)
         }
 
         return outboundBean

parhelia512/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/fmt/HttpFmt.kt
@@ -4,6 +4,7 @@ import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.dto.V2rayConfig.OutboundBean
 import com.neko.v2ray.extension.isNotNullEmpty
+import com.neko.v2ray.handler.V2rayConfigManager
 
 object HttpFmt : FmtBase() {
     /**
@@ -13,7 +14,7 @@ object HttpFmt : FmtBase() {
      * @return the converted OutboundBean object, or null if conversion fails
      */
     fun toOutbound(profileItem: ProfileItem): OutboundBean? {
-        val outboundBean = OutboundBean.create(EConfigType.HTTP)
+        val outboundBean = V2rayConfigManager.createOutbound(EConfigType.HTTP)
 
         outboundBean?.settings?.servers?.first()?.let { server ->
             server.address = profileItem.server.orEmpty()

V2rayNG/app/src/main/java/com/neko/v2ray/fmt/SocksFmt.kt
@@ -5,6 +5,7 @@ import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.dto.V2rayConfig.OutboundBean
 import com.neko.v2ray.extension.idnHost
 import com.neko.v2ray.extension.isNotNullEmpty
+import com.neko.v2ray.handler.V2rayConfigManager
 import com.neko.v2ray.util.Utils
 import java.net.URI
 
@@ -60,7 +61,7 @@ object SocksFmt : FmtBase() {
      * @return the converted OutboundBean object, or null if conversion fails
      */
     fun toOutbound(profileItem: ProfileItem): OutboundBean? {
-        val outboundBean = OutboundBean.create(EConfigType.SOCKS)
+        val outboundBean = V2rayConfigManager.createOutbound(EConfigType.SOCKS)
 
         outboundBean?.settings?.servers?.first()?.let { server ->
             server.address = profileItem.server.orEmpty()

V2rayNG/app/src/main/java/com/neko/v2ray/fmt/VlessFmt.kt
@@ -6,6 +6,7 @@ import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.dto.V2rayConfig.OutboundBean
 import com.neko.v2ray.extension.idnHost
 import com.neko.v2ray.handler.MmkvManager
+import com.neko.v2ray.handler.V2rayConfigManager
 import com.neko.v2ray.util.Utils
 import java.net.URI
 
@@ -56,7 +57,7 @@ object VlessFmt : FmtBase() {
      * @return the converted OutboundBean object, or null if conversion fails
      */
     fun toOutbound(profileItem: ProfileItem): OutboundBean? {
-        val outboundBean = OutboundBean.create(EConfigType.VLESS)
+        val outboundBean = V2rayConfigManager.createOutbound(EConfigType.VLESS)
 
         outboundBean?.settings?.vnext?.first()?.let { vnext ->
             vnext.address = profileItem.server.orEmpty()
@@ -67,35 +68,11 @@ object VlessFmt : FmtBase() {
         }
 
         val sni = outboundBean?.streamSettings?.let {
-            populateTransportSettings(
-                it,
-                profileItem.network.orEmpty(),
-                profileItem.headerType,
-                profileItem.host,
-                profileItem.path,
-                profileItem.seed,
-                profileItem.quicSecurity,
-                profileItem.quicKey,
-                profileItem.mode,
-                profileItem.serviceName,
-                profileItem.authority,
-                profileItem.xhttpMode,
-                profileItem.xhttpExtra
-            )
+            V2rayConfigManager.populateTransportSettings(it, profileItem)
         }
 
         outboundBean?.streamSettings?.let {
-            populateTlsSettings(
-                it,
-                profileItem.security.orEmpty(),
-                profileItem.insecure == true,
-                if (profileItem.sni.isNullOrEmpty()) sni else profileItem.sni,
-                profileItem.fingerPrint,
-                profileItem.alpn,
-                profileItem.publicKey,
-                profileItem.shortId,
-                profileItem.spiderX,
-            )
+            V2rayConfigManager.populateTlsSettings(it, profileItem, sni)
         }
 
         return outboundBean

V2rayNG/app/src/main/java/com/neko/v2ray/fmt/VmessFmt.kt
@@ -11,6 +11,7 @@ import com.neko.v2ray.dto.VmessQRCode
 import com.neko.v2ray.extension.idnHost
 import com.neko.v2ray.extension.isNotNullEmpty
 import com.neko.v2ray.handler.MmkvManager
+import com.neko.v2ray.handler.V2rayConfigManager
 import com.neko.v2ray.util.JsonUtil
 import com.neko.v2ray.util.Utils
 import java.net.URI
@@ -168,7 +169,7 @@ object VmessFmt : FmtBase() {
      * @return the converted OutboundBean object, or null if conversion fails
      */
     fun toOutbound(profileItem: ProfileItem): OutboundBean? {
-        val outboundBean = OutboundBean.create(EConfigType.VMESS)
+        val outboundBean = V2rayConfigManager.createOutbound(EConfigType.VMESS)
 
         outboundBean?.settings?.vnext?.first()?.let { vnext ->
             vnext.address = profileItem.server.orEmpty()
@@ -178,35 +179,11 @@ object VmessFmt : FmtBase() {
         }
 
         val sni = outboundBean?.streamSettings?.let {
-            populateTransportSettings(
-                it,
-                profileItem.network.orEmpty(),
-                profileItem.headerType,
-                profileItem.host,
-                profileItem.path,
-                profileItem.seed,
-                profileItem.quicSecurity,
-                profileItem.quicKey,
-                profileItem.mode,
-                profileItem.serviceName,
-                profileItem.authority,
-                profileItem.xhttpMode,
-                profileItem.xhttpExtra
-            )
+            V2rayConfigManager.populateTransportSettings(it, profileItem)
         }
 
         outboundBean?.streamSettings?.let {
-            populateTlsSettings(
-                it,
-                profileItem.security.orEmpty(),
-                profileItem.insecure == true,
-                if (profileItem.sni.isNullOrEmpty()) sni else profileItem.sni,
-                profileItem.fingerPrint,
-                profileItem.alpn,
-                null,
-                null,
-                null
-            )
+            V2rayConfigManager.populateTlsSettings(it, profileItem, sni)
         }
 
         return outboundBean