293d5921fc72b46191d86de8009ef568cf5444b8
AnGgIt886 <jarbull86@gmail.com>
2025-07-01 20:08:06 +0700

    Neko-ray `v1.10.8`

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.neko.v2ray"
         minSdk = 27
         targetSdk = 35
-        versionCode = 652
-        versionName = "1.10.2"
+        versionCode = 658
+        versionName = "1.10.8"
         multiDexEnabled = true
         vectorDrawables.useSupportLibrary = true
 
